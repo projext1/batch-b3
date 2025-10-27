c151168d59506f65e1768defd5b952707df6f4c1
projext1 <t230333@famt.ac.in>
2025-10-27 11:52:00 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
-public class HelloWorld {
+public class helloworld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, Jenkins!");
     }
 }
+
+
